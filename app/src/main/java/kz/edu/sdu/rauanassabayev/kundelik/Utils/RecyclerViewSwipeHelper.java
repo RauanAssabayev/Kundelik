7a7fc3e6e5492b830b254267f28c0c86c7f0519d
rauanassabayev <rauan.assabayev@gmail.com>
2017-11-23 00:02:36 +0600

    Added select subject

--- app/src/main/java/kz/edu/sdu/rauanassabayev/kundelik/Utils/RecyclerViewSwipeHelper.java
+++ app/src/main/java/kz/edu/sdu/rauanassabayev/kundelik/Utils/RecyclerViewSwipeHelper.java
@@ -33,6 +33,7 @@ public class RecyclerViewSwipeHelper extends ItemTouchHelper.Callback {
     public boolean isLongPressDragEnabled() {
         return true;
     }
+
     @Override
     public boolean isItemViewSwipeEnabled() {
         return true;
